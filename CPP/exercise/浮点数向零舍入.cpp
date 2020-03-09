36bb889841294196da8ed6a2556dbb689a819e48
ShyButHandsome <shybuthandsome@outlook.com>
2020-03-09 21:51:34 +0800

    浮点数向零舍入

--- CPP/exercise/浮点数向零舍入.cpp
+++ CPP/exercise/浮点数向零舍入.cpp
@@ -1,2 +1,14 @@
 // Input an float number
-// 负数向上，正数向下
+// 负数向上，正数向下（说白了都是舍去小数部分）
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double input;
+    cin >> input;
+
+    cout << static_cast<int>(input);
+}
+
+// 强制类型转换可以实现 
